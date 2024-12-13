d31c392f2472e6af1232994ed21022ae4845bbac
han <hcsy1219@gmail.com>
2024-12-13 09:50:07 +0900

    Init: Gateway part project upload

---
+++ src/main/kotlin/kan9hee/nolaejui_gateway/NolaejuiGatewayApplication.kt
@@ -0,0 +1,11 @@
+package kan9hee.nolaejui_gateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NolaejuiGatewayApplication
+
+fun main(args: Array<String>) {
+	runApplication<NolaejuiGatewayApplication>(*args)
+}
---
+++ src/test/kotlin/kan9hee/nolaejui_gateway/NolaejuiGatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package kan9hee.nolaejui_gateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NolaejuiGatewayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
